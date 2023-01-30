59efa9e07ef0f6bf6d330efaecce7e88e0f36724
DESKTOP-0G88Q6M\KGA_18 <asd5851@naver.com>
2023-01-30 14:59:57 +0900

    [kive] setup title scene

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Copy mobile tangram game
 2023-01-30 / v0.0.1 / Init Tangram game project    
 2023-01-30 / v0.0.2 / Init Tmp fonts    
 2023-01-30 / v0.1.0 / Setup Tangram game project    
+2023-01-30 / v0.1.1 / Setup Title Scene    
+
---
+++ TangramGame/Assets/01.UnityProject/Scripts/Runtime/PlayBtn.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBtn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //! 플레이 버튼을 눌렀을 떄 플레이 씬으로 넘어간다.
+    public void OnPlayButton()
+    {
+        GFunc.LoadScene(GData.SCENE_NAME_PLAY);
+    }       //OnPlayButton
+}
